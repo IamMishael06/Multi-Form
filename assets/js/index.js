cb9fce9573f587f757999555172a2a329e578739
Tari Namikpoh <tarinamikpoh@gmail.com>
2023-12-08 06:43:08 +0100

    first commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,25 @@
 function animationActivities(params) {
     var steps = document.querySelectorAll('.step')
+    var back = document.querySelector('.go-back')
+    var hasBeenClicked = false
     steps.forEach( step => {
         step.addEventListener('click',(e)=>{
-            step.classList.add('active')
+            steps.forEach( OtherStep => {
+                if (OtherStep  !== step && OtherStep.classList.contains('active')) {
+                    step.classList.remove('active')
+                }else{
+                    step.classList.add('active')
+                }
+            });
+
+            if (!e.target.classList.contains('first')) {
+                back.classList.add('active')
+            }else{
+                back.classList.remove('active')
+            }
         })
+      
     });
+   
 }
 animationActivities();
